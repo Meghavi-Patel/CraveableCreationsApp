18d2327c82b7b05621d858c581463a103ec97020
Meghavi <meghavipatel@outlookemail.ca>
2025-05-05 17:32:32 -0400

    Project setup

---
+++ craveable_creations.backend.app/src/main/java/com/craveable_creations/backend/app/CraveableCreationsApp.java
@@ -0,0 +1,13 @@
+package com.craveable_creations.backend.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraveableCreationsApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CraveableCreationsApp.class, args);
+	}
+
+}
